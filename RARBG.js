008e98c4df8b82b1ddd4fcf37bd6614d3aaa3378
George Parks <60108810+geoge23@users.noreply.github.com>
2021-02-17 11:59:00 -0500

    added download features

--- RARBG.js
+++ RARBG.js
@@ -4,40 +4,55 @@ module.exports = class {
     constructor(appName) {
         this.appName = appName;
     }
-    async getToken() {
-        const {data} = await axios.get('https://torrentapi.org/pubapi_v2.php', {
+    async _getToken() {
+        const {data, headers} = await axios.get('https://torrentapi.org/pubapi_v2.php', {
             params: {
                 get_token: 'get_token',
                 app_id: this.appName
             }
         })
+        await this._wait(200)
         return data.token;
     }
-    async search({name, season = 1, episode = 1, resolution = 720, limit = 5, category = 'tv'}) {
+    _wait(time) {
+        return new Promise((res, _) => {
+            setTimeout(() => res(), time)
+        })
+    }
+    async search({name, season = 1, episode = 1, res = 720, limit = 5, category = 'tv'}) {
         try {
-            if (this.token == undefined) this.token = await this.getToken()
-            return (await axios.get('https://torrentapi.org/pubapi_v2.php', {
+            if (this.token == undefined) this.token = await this._getToken()
+            let data = (await axios.get('https://torrentapi.org/pubapi_v2.php', {
                 params: {
                     app_id: this.appName,
                     token: this.token,
-                    category: this.resolveCategories(category),
-                    search_string: this.generateSearch(name, season, episode, resolution),
+                    category: this._resolveCategories(category),
+                    search_string: this._generateSearch(name, season, episode, res, category),
                     mode: 'search',
                     sort: 'seeders',
                     limit
                 }
             })).data
+            if (data.torrent_results) {
+                return data.torrent_results
+            } else {
+                return data
+            }
         } catch (e) {
             console.log(e)
         }
     }
-    generateSearch(name, season, episode, resolution) {
+    _generateSearch(name, season, episode, resolution, category) {
         season = season.toString()
         episode = episode.toString()
         resolution = resolution.toString()
-        return `${name} S${season.padStart(2, '0')}E${episode.padStart(2, '0')} ${resolution}p`
+        if (category == 'movie') {
+            return `${name} ${resolution}p`
+        } else {
+            return `${name} S${season.padStart(2, '0')}E${episode.padStart(2, '0')} ${resolution}p`
+        }
     }
-    resolveCategories(type) {
+    _resolveCategories(type) {
         switch (type) {
             case 'tv':
                 return '1;18;41;49'
--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const app = express();
 const RARBG = require('./RARBG')
+const torrents = {}
 
 const rarbg = new RARBG('DST');
 app.use(require('body-parser').json())
 app.set('view engine', 'ejs')
 require('dotenv').config()
 
-rarbg.search({name: 'Agents of SHIELD'}).then(e => console.log(e))
 
 app.get('/', (req, res) => {
     res.render('./index.ejs')
 })
 
-app.post('/torrents', (req, res) => {
-    console.log(req.body)
+app.post('/torrents', async (req, res) => {
+    const body = req.body;
+    const search = await rarbg.search({
+        name: body.name,
+        category: body.category,
+        episode: body.episode || 1,
+        season: body.season || 1,
+        res: this.res
+    })
+    if (search instanceof Array) search.forEach(({filename, download}) => {
+        torrents[filename] = download;
+    })
+    res.status(200).send(search)
+})
+
+app.post('/download', async (req, res) => {
+    const magnet = torrents[req.body.name]
+    res.status(200).send()
 })
 
 app.listen(process.env.PORT || 8080)
