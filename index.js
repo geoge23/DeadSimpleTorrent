a532c9d940196896c63224164cf733ba689ba559
George Parks <60108810+geoge23@users.noreply.github.com>
2021-02-17 12:38:14 -0500

    finished torrent system

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express')
 const app = express();
 const RARBG = require('./RARBG')
@@ -22,15 +23,67 @@ app.post('/torrents', async (req, res) => {
         season: body.season || 1,
         res: this.res
     })
-    if (search instanceof Array) search.forEach(({filename, download}) => {
-        torrents[filename] = download;
+    if (search instanceof Array) search.forEach(({filename: name, download}) => {
+        torrents[name] = {download, name, category: body.category};
     })
     res.status(200).send(search)
 })
 
 app.post('/download', async (req, res) => {
-    const magnet = torrents[req.body.name]
-    res.status(200).send()
+    try {
+        const {download, name, category} = torrents[req.body.name]
+        await axios({
+            method: 'POST',
+            url: process.env.TRANSMISSION_URL,
+            data: {
+                "method": "torrent-add",
+                "arguments": {
+                    "paused": false,
+                    "download-dir": generateDownloadDir(name, category),
+                    "filename": download
+                }
+            },
+            headers: {
+                'x-transmission-session-id': await getSessionID()
+            },
+            auth: {
+                username: process.env.TRANSMISSION_USERNAME,
+                password: process.env.TRANSMISSION_PASSWORD,
+            }
+        })
+        res.status(200).send()
+    } catch (e) {
+        console.log(e)
+        res.status(500).send()
+    }
 })
+
+function generateDownloadDir(name, category) {
+    let path = '';
+    if (category == 'tv') {
+        name = name.split('.S')
+        path += name[0]
+        path += '/S'
+        path += name[1].split('E')[0]
+    } else {
+        path = 'tv'
+    }
+    return `/data/completed/${category}/${path}`;
+}
+
+async function getSessionID() {
+    try {
+        return (await axios({
+            method: 'POST',
+            url: process.env.TRANSMISSION_URL,
+            auth: {
+                username: process.env.TRANSMISSION_USERNAME,
+                password: process.env.TRANSMISSION_PASSWORD,
+            }
+        })).headers['x-transmission-session-id']
+    } catch (e) {
+        return e.response.headers['x-transmission-session-id']
+    }
+}
 
 app.listen(process.env.PORT || 8080)
